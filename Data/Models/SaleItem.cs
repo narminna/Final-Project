cc2cd58d93f0ed9a8eed7aa7dbff291c9357b6dd
unknown <narminna@code.edu.az>
2023-07-25 21:41:59 +0400

    Inteface has been created and some details in models changed

---
+++ Data/Models/SaleItem.cs
@@ -0,0 +1,27 @@
+﻿using Final_Project.Data.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Project.Data.Models
+{
+    public class SaleItem : BaseEntity
+    {
+        private static int count = 0;
+
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+
+        public SaleItem(Product product, int quantity)
+        {
+            Product = product;
+            Quantity = quantity;
+
+            ID = count;
+            count++;
+        }
+
+    }
+}
---
+++ Services/Concrete/MenuServices.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Project.Services.Concrete
+{
+    public class MenuServices
+    {
+        public static void MenuAddProduct()
+        {
+			try
+			{
+
+			}
+			catch (Exception ex)
+			{
+                Console.WriteLine($"OOPS, got an error!{ex.Message}");
+            }
+        }
+
+        public static void MenuUpdatePorduct()
+        {
+
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"OOPS, got an error!{ex.Message}");
+            }
+        }
+
+        public static void MenuRemoveProduct()
+        {
+
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"OOPS, got an error!{ex.Message}");
+            }
+        }
+
+        public static void MenuShowAllProduct()
+        {
+
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"OOPS, got an error!{ex.Message}");
+            }
+        }
+
+        public static void MenuChooseProductByCategory()
+        {
+
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"OOPS, got an error!{ex.Message}");
+            }
+        }
+
+        public static void MenuPriceRangeProduct()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"OOPS, got an error!{ex.Message}");
+            }
+        }
+
+        public static void MenuSearchProductByName()
+        {
+
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"OOPS, got an error!{ex.Message}");
+            }
+        }
+
+        public static void MenuAddSales()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"OOPS, got an error!{ex.Message}");
+            }
+        }
+
+        public static void MenuReturnSales()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"OOPS, got an error!{ex.Message}");
+            }
+        }
+
+        public static void MenuRemoveSales()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"OOPS, got an error!{ex.Message}");
+            }
+        }
+
+        public static void MenuShowAllSales()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"OOPS, got an error!{ex.Message}");
+            }
+        }
+
+        public static void MenuShowSalesByDate()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"OOPS, got an error!{ex.Message}");
+            }
+        }
+
+        public static void MenuShowSalesByPrice()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"OOPS, got an error!{ex.Message}");
+            }
+        }
+
+        public static void MenuShowSalesByGivenDate()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"OOPS, got an error!{ex.Message}");
+            }
+        }
+
+        public static void MenuShowSalesByGivenID()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"OOPS, got an error!{ex.Message}");
+            }
+        }
+
+        public static void MenuGoBack()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"OOPS, got an error!{ex.Message}");
+            }
+        }
+    }
+}
